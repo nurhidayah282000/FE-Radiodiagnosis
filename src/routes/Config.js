b63c85cf5601aea416e2f8e529a1defaab108655
= <=>
2023-06-08 08:37:14 +0700

    fix: change prod url

--- src/routes/Config.js
+++ src/routes/Config.js
@@ -1,2 +1,2 @@
-// export const baseURL = "https://api.radiodiagnosis-report.me"
-export const baseURL = "http://localhost:5000"
+export const baseURL = "https://api.radiodiagnosis-report.me"
+// export const baseURL = "http://localhost:5000"
